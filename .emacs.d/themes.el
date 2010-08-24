d7f6ce5bac69f5660c6a9d3ac4c29e1ed1f2bd13
Jordan Ritter <jpr5@darkridge.com>
2010-08-24 01:39:15 -0700

    Add a midnight theme variant and start using that instead.

--- .emacs.d/themes.el
+++ .emacs.d/themes.el
@@ -1,21 +1,37 @@
 (require 'color-theme)
 
-(defun color-theme-jpr5-gui ()
-  "jpr5's gui theme"
+;; For some reason, Emacs kept loading the source along with the elcs.
+;(setq load-path (add-to-list 'load-path "/Applications/Emacs.app/Contents/Resources/site-lisp/color-theme/themes"))
+;(load-library "color-theme-example.elc")
+;(load-library "color-theme-library.elc")
+
+(defun color-theme-jpr5-tty ()
+  "jpr5's tty theme"
   (interactive)
   (color-theme-install
-    '(color-theme-jpr5-gui
+    '(color-theme-jpr5-tty
+      ((foreground-color . "white") (background-color . "black") (background-mode . dark))
+
+      (font-lock-comment-face           ((t (:foreground "dark red"))))
+      (font-lock-comment-delimiter-face ((t (:foreground "dark red"))))
+      (font-lock-string-face            ((t (:foreground "light grey"))))
+      (font-lock-warning-face           ((t (:foreground "black" :background "yello"))))
+      (region                           ((t (:foreground nil     :background "dark grey"))))
+      )))
+
+(defun color-theme-jpr5-day ()
+  "jpr5's daytime theme"
+  (interactive)
+  (color-theme-install
+    '(color-theme-jpr5-day
       ((foreground-color . "black") (background-color . "grey75") (cursor-color . "DarkBlue") (background-mode . light))
 
       (font-lock-warning-face           ((t (:foreground "red" :bold t :italic nil :underline t))))
       (font-lock-comment-face           ((t (:foreground "dark red" :italic t))))
       (font-lock-comment-delimiter-face ((t (:foreground "dark red" :italic t))))
 
-;      (font-lock-string-face            ((t (:foreground "lightyellow1"))))
       (font-lock-string-face            ((t (:foreground "grey35" :background "grey78"))))
-      ;(font-lock-variable-name-face     ((t (:foreground "black" :bold t))))
       (font-lock-variable-name-face     ((t (:foreground "slateblue4" :bold t))))
-;      (font-lock-type-face              ((t (:foreground "firebrick3" :bold t))))
       (font-lock-type-face              ((t (:foreground "forest green" :bold nil))))
       (font-lock-constant-face          ((t (:foreground "slateblue3" :bold t))))
       (font-lock-builtin-face           ((t (:foreground "dark blue"))))
@@ -31,17 +47,38 @@
       (show-paren-match-face            ((t (:foreground "white" :background "grey50" :bold t))))
       )))
 
-(defun color-theme-jpr5-tty ()
-  "jpr5's tty theme"
+
+(defun color-theme-jpr5-night ()
+  "jpr5's night time theme"
   (interactive)
   (color-theme-install
-    '(color-theme-jpr5-tty
-      ((foreground-color . "white") (background-color . "black") (background-mode . dark))
+   '(color-theme-jpr5-night
+     ((font . "fixed")
+      (width . 130)
+      (height . 50)
+      (background-color . "black")
+      (foreground-color . "grey85")
+      (background-mode . dark)
+      (mouse-color . "grey85")
+      (cursor-color . "grey85"))
+     (default ((t (nil))))
+     (font-lock-comment-face ((t (:italic t :foreground "grey60"))))
+     (font-lock-string-face ((t (:foreground "Magenta"))))
+     (font-lock-keyword-face ((t (:foreground "Cyan"))))
+     (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
+     (font-lock-constant-face ((t (:foreground "OliveDrab"))))
+     (font-lock-type-face ((t (:foreground "DarkCyan"))))
+     (font-lock-variable-name-face ((t (:foreground "DarkGoldenrod"))))
+     (font-lock-function-name-face ((t (:foreground "SlateBlue"))))
+     (font-lock-builtin-face ((t (:foreground "SkyBlue"))))
+     (highline-face ((t (:background "grey12"))))
+     (hl-line       ((t (:background "grey12"))))
+     (setnu-line-number-face ((t (:background "Grey15" :foreground "White" :bold t))))
+     (show-paren-match-face ((t (:background "grey30"))))
+     (region ((t (:background "grey15"))))
+     (highlight ((t (:background "blue"))))
+     (secondary-selection ((t (:background "navy"))))
+     (widget-field-face ((t (:background "navy"))))
+     (widget-single-line-field-face ((t (:background "royalblue")))))) )
 
-      (font-lock-comment-face           ((t (:foreground "dark red"))))
-      (font-lock-comment-delimiter-face ((t (:foreground "dark red"))))
-      (font-lock-string-face            ((t (:foreground "light grey"))))
-      (font-lock-warning-face           ((t (:foreground "black" :background "yello"))))
-      (region                           ((t (:foreground nil     :background "dark grey"))))
-      )))
 
