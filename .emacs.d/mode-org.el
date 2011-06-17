5976b7eceb58e57662cfdcae9350b3c52da43ff8
Jordan Ritter <jpr5@darkridge.com>
2011-06-17 09:12:51 -0700

    Remap a few org-mode keys in emacs.

--- .emacs.d/mode-org.el
+++ .emacs.d/mode-org.el
@@ -8,6 +8,13 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 
+(add-hook 'org-mode-hook
+          (lambda ()
+            (local-unset-key (kbd "M-<return>")) ; for fullscreen in keys.el
+            (local-set-key (kbd "<return>") 'org-return)
+            (local-set-key (kbd "M-n") 'outline-next-visible-heading)
+            (local-set-key (kbd "M-p") 'outline-previous-visible-heading)))
+
 (setq org-log-done 'time)
 (setq org-deadline-warning-days 10)
 ;(setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
