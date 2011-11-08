c0f5aca32297284d26f9ffe66945131b0a1b05a0
Jordan Ritter <jpr5@darkridge.com>
2011-11-08 00:05:52 -0800

    Made trailing-whitespace cleanup optional, and added cycle-through-tab-settings.

--- .emacs.d/tabs.el
+++ .emacs.d/tabs.el
@@ -1,12 +1,25 @@
 ;; TAB settings
 ;;
-;; Set some basic requirements around input (width 4, don't use tabs), but then also
-;; define some magic that will replace tabs if we happen to save a file that has tabs
-;; inadvertently added to it, as well as nuke trailing whitespace.  This is the default
-;; behaviour; (setq indent-tabs-mode t) in a hook for modes you want left alone.
+;; Set some basic requirements around input (width 4, don't use tabs), but then
+;; also define some magic that will replace tabs if we happen to save a file
+;; that has tabs inadvertently added to it, as well as nuke trailing whitespace.
+;; This is the default behaviour; (setq indent-tabs-mode t) in a hook for modes
+;; you want left alone.
 
 (setq-default tab-width        4)   ;; used by untabify
 (setq-default indent-tabs-mode nil)
+
+
+(setq-default delete-trailing-whitespace-mode t)
+
+(add-hook 'write-file-hooks
+  (lambda ()
+    (unless indent-tabs-mode)
+     (save-excursion
+      (untabify (point-min) (point-max))
+      (when delete-trailing-whitespace-mode
+        (delete-trailing-whitespace))
+      )))
 
 ;; If indent-tabs-mode is off, nuke whitespace and untabify before saving.  I do this by
 ;; default now, with modes "opting out" of the practice by setting indent-tabs-mode to
@@ -17,14 +30,24 @@
 ;    (if (not indent-tabs-mode)
 ;      (save-excursion
 ;        (untabify (point-min) (point-max))
-;        (delete-trailing-whitespace)
+;        (if delete-trailing-whitespace-mode
+;             (delete-trailing-whitespace))
 ;        ))))
 
-(add-hook 'write-file-hooks
-  (lambda ()
-    (if (not indent-tabs-mode)
-      (save-excursion
-        (untabify (point-min) (point-max))
-        (delete-trailing-whitespace)
-        ))))
+;; Mechanism to cycle through N tab width settings.  Expected to be mapped to a
+;; keybinding.  This is effectively a toggle when the list is of only 2 values.
+;; Automatically becomes buffer local; take out make-.. if for some reason you
+;; don't want that.
+
+(setq-default tab-widths '(2 4))
+
+(defun cycle-tab-width ()
+  "Cycle through tab widths"
+  (interactive)
+  (make-variable-buffer-local 'tab-widths)
+  (setq tab-width (pop tab-widths))
+  (nconc tab-widths (list tab-width))
+  (setq ruby-indent-level tab-width)
+  (message "Tab-width set to %d." tab-width)
+  (redraw-display))
 
