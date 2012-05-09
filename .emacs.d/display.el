ccbd51464e64d6492db26e9bfdb86511085311c9
Jordan Ritter <jpr5@darkridge.com>
2012-05-08 18:17:55 -0700

    Re-enable menu bar now that this all works with Cocoa Emacs.

--- .emacs.d/display.el
+++ .emacs.d/display.el
@@ -4,7 +4,7 @@
 (setq emacs-query-startup t)
 (setq emacs-base-display t)
 
-(if (fboundp 'menu-bar-mode)   (menu-bar-mode -1))
+;(if (fboundp 'menu-bar-mode)   (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode)   (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
